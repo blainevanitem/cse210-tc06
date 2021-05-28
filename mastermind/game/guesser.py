04897d2839c47bcdcd913ed474bd026b950c208a
Blaine <van17021@byui.edu>
2021-05-28 14:48:32 -0600

    changes to initial program

--- mastermind/game/guesser.py
+++ mastermind/game/guesser.py
@@ -1,3 +1,8 @@
 class Guesser():
     def __init__(self):
-        
+        self._player1_data = ["----","****"]
+        self._player2_data = ["----","****"]
+
+    def get_guess(self):
+        pass
+    
